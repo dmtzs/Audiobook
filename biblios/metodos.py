b25adab91ff1cbe03334d7fce19d5f445354be8b
Diego MS <diego.mtzs00@gmail.com>
2021-09-13 19:54:38 -0500

    Creation of local library
    Cration of a local library in order to modulate more the code

---
+++ biblios/metodos.py
@@ -0,0 +1,38 @@
+try:
+    import platform
+    import os
+except ImportError as eImp:
+    print(f"Ocurriò el siguiente ERROR de importación: {eImp}")
+
+class funciones():
+    sis= ""
+    comm= ""
+
+    def __init__(self, mainFrame):
+        self.mainWin= mainFrame
+
+    def commandSO(self):
+        sistema= platform.system()
+
+        if sistema== "Windows":
+            return "cls", sistema
+        else:
+            return "clear", sistema
+
+    def configWindow(self):
+        self.mainWin.title("Audiolibro")
+        self.mainWin.resizable(width= False, height= False)
+        self.mainWin.iconbitmap("./Audiolib.ico")
+        screenWidth = self.mainWin.winfo_screenwidth()# Ancho del área de visualización
+        screenHeight = self.mainWin.winfo_screenheight()# Alto del área de visualización
+        self.comm, self.sis= self.commandSO()
+
+        if self.sis== "Windows":
+            width= 500
+            height= 550
+        else:
+            width= 1000
+            height= 1050
+        left = (screenWidth - width) / 2
+        top = (screenHeight - height) / 2
+        self.mainWin.geometry(f"{int(width)}x{int(height)}+{int(left)}+{int(top)}")
