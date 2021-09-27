d581fbafc9c8f48230a3ec80e64efb8914bfb740
Diego Ms <diego.mtzs00@gmail.com>
2021-09-27 09:49:47 -0500

    Update tkAudiolib.py
    Added error managers to the main program

--- tkAudiolib.py
+++ tkAudiolib.py
@@ -8,7 +8,13 @@ if __name__== "__main__":
     mainTitleApp= "Audiobook"
     titleApp= "Audiobook app"
 
-    # Instancia de la clase dentro de bilbios
-    met= metodos.funciones(titleApp, mainTitleApp)
-    
-    met.GUI()
+    try:
+        # Llamadas a la GUI
+        met= metodos.funciones(titleApp, mainTitleApp)
+        met.GUI()
+    except KeyboardInterrupt:
+        print("Se presionó Ctrl + C")
+    except Exception as ex:
+        print(f"Ocurrió el siguiente ERROR: {ex}")
+    finally:
+        print("Finalizando programa")
