e029d52d771959c7854ae8e8375e020c844262d1
Diego Ms <diego.mtzs00@gmail.com>
2021-10-08 10:31:21 -0500

    Update metodos.py
    Added messagebox dialogs for validate the formulary in the same application

--- biblios/metodos.py
+++ biblios/metodos.py
@@ -59,10 +59,18 @@ class extraMethods():
                 speaker.save_to_file(pdftxtReader, f"{self.folderName}/{outputname}.mp3")
                 speaker.runAndWait()
 
-            messagebox.showinfo("Éxito", "Archivo convertido en audio con éxito")
+            messagebox.showinfo("Éxito", "File converted in audio format with success!!")
 
         except Exception:
-            messagebox.showerror("Error", "Algo falló al leer el archivo, asegurate de que sea un PDF o TXT")
+            messagebox.showerror("ERROR", "Something failed at the moment we try to reads the file, be sure that is a PDF or TXT file")
+
+    def messageDialog(self, nameFile):
+        if nameFile== "":
+            messagebox.showerror("ERROR", "The name of the file shouldn´t be empty")
+
+            return 1
+        else:
+            return 0
 
 class funciones(extraMethods):
     sis= ""
@@ -70,7 +78,6 @@ class funciones(extraMethods):
     fileName= ""
     folderName= ""
     fileIco= "Audiolib.ico"
-    mainWin= tk.Tk()
 
     def __init__(self, titleApp, mainTitleApp):
         self.titleApp= titleApp
@@ -114,20 +121,26 @@ class funciones(extraMethods):
                 for elem in range(len(txtLines)):
                     txtLines[elem]= txtLines[elem][:-1]
                 self.generateAudio(speedRateDes-15.4, outputname, spElec, txtLines, ext)
-                
+
             else:
                 messagebox.showerror("ERROR", "You should select only TXT and/or PDF files")
 
+        def validateEntryName(*args):
+            nameFile= fileNameEntry.get()
+
+            self.messageDialog(nameFile)
+
         # ----------------Instrucciones de la GUI principal.----------------
-        self.mainWin.title(self.titleApp)
-        self.mainWin.resizable(width= False, height= False)
+        mainWin= tk.Tk()
+        mainWin.title(self.titleApp)
+        mainWin.resizable(width= False, height= False)
         try:
             imaIco= self.resource_path(self.fileIco)
-            self.mainWin.iconbitmap(imaIco)
+            mainWin.iconbitmap(imaIco)
         except Exception:
-            self.mainWin.iconbitmap("Audiolib.ico")
-        screenWidth = self.mainWin.winfo_screenwidth()# Ancho del área de visualización
-        screenHeight = self.mainWin.winfo_screenheight()# Alto del área de visualización
+            mainWin.iconbitmap("Audiolib.ico")
+        screenWidth = mainWin.winfo_screenwidth()# Ancho del área de visualización
+        screenHeight = mainWin.winfo_screenheight()# Alto del área de visualización
         self.comm, self.sis= self.commandSO()
 
         if self.sis== "Windows":
@@ -138,77 +151,78 @@ class funciones(extraMethods):
             height= 1050
         left = (screenWidth - width) / 2
         top = (screenHeight - height) / 2
-        self.mainWin.geometry(f"{int(width)}x{int(height)}+{int(left)}+{int(top)}")
+        mainWin.geometry(f"{int(width)}x{int(height)}+{int(left)}+{int(top)}")
 
         # ----------------Componentes del frame----------------
         # ----------------Rutas----------------
-        titleLabel= tk.Label(self.mainWin, fg= "purple", text= self.mainTitleApp, font= ("jost", 25))
+        titleLabel= tk.Label(mainWin, fg= "purple", text= self.mainTitleApp, font= ("jost", 25))
         titleLabel.place(relx= 0.5, y= 25, anchor= CENTER)
 
-        fileLabel= tk.Label(self.mainWin, fg= "black", text= "Choose file to read", font= ("jost", 15))
+        fileLabel= tk.Label(mainWin, fg= "black", text= "Choose file to read", font= ("jost", 15))
         fileLabel.place(relx= 0.5, y= 78, anchor= CENTER)
 
-        rutaError= tk.Label(self.mainWin, fg= "red", text= "Select a file", font= ("jost", 8))
+        rutaError= tk.Label(mainWin, fg= "red", text= "Select a file", font= ("jost", 8))
         rutaError.place(x= 90, y= 102)
 
-        saveEntry= tk.Button(self.mainWin, fg= "white", width= 10, bg= "#794ECF", text= "File", takefocus= False, command= abrirRuta)
+        saveEntry= tk.Button(mainWin, fg= "white", width= 10, bg= "#794ECF", text= "File", takefocus= False, command= abrirRuta)
         saveEntry.place(x= 10, y= 100)
 
-        fileLabel= tk.Label(self.mainWin, fg= "black", text= "Choose path to save output file", font= ("jost", 15))
+        fileLabel= tk.Label(mainWin, fg= "black", text= "Choose path to save output file", font= ("jost", 15))
         fileLabel.place(relx= 0.5, y= 150, anchor= CENTER)
 
-        rutaError2= tk.Label(self.mainWin, fg= "red", text= "Select a path", font= ("jost", 8))
+        rutaError2= tk.Label(mainWin, fg= "red", text= "Select a path", font= ("jost", 8))
         rutaError2.place(x= 90, y= 182)
 
-        saveEntry2= tk.Button(self.mainWin, fg= "white", width= 10, bg= "#794ECF", text= "Path", takefocus= False, command= abrirRuta2)
+        saveEntry2= tk.Button(mainWin, fg= "white", width= 10, bg= "#794ECF", text= "Path", takefocus= False, command= abrirRuta2)
         saveEntry2.place(x= 10, y= 180)
 
         # ----------------Resto de componentes----------------
-        velocidadLabel= tk.Label(self.mainWin, fg= "black", text= "Enter speed rate of the voice:", font= ("jost", 13))
+        velocidadLabel= tk.Label(mainWin, fg= "black", text= "Enter speed rate of the voice:", font= ("jost", 13))
         velocidadLabel.place(x= 10, y= 230)
 
         spinVelVar= tk.StringVar()
         spinVelVar.set("130")
-        spinVelocidad= tk.Spinbox(self.mainWin, from_= 1, to= 200, width= 15, textvariable= spinVelVar)
+        spinVelocidad= tk.Spinbox(mainWin, from_= 1, to= 200, width= 15, textvariable= spinVelVar)
         spinVelocidad.place(x= 240, y= 233)
 
-        fileNameLabel= tk.Label(self.mainWin, fg= "black", text= "Enter the name of the mp3 file:", font= ("jost", 13))
+        fileNameLabel= tk.Label(mainWin, fg= "black", text= "Enter the name of the mp3 file:", font= ("jost", 13))
         fileNameLabel.place(x= 10, y= 270)
 
         fileNameEntryText= tk.StringVar()
         fileNameEntryText.set("output")
-        fileNameEntry= tk.Entry(self.mainWin, width= 40, textvariable= fileNameEntryText)
+        fileNameEntry= tk.Entry(mainWin, width= 40, textvariable= fileNameEntryText)
         fileNameEntry.focus()
         fileNameEntry.place(x= 240, y= 273.4)
+        fileNameEntryText.trace_add("write", validateEntryName)
 
-        guardLabel= tk.Label(self.mainWin, fg= "black", text= "Save and play or just save the audio file?:", font= ("jost", 13))
+        guardLabel= tk.Label(mainWin, fg= "black", text= "Save and play or just save the audio file?:", font= ("jost", 13))
         guardLabel.place(x= 10, y= 315)
 
         valores= ["Save only", "Save and play"]
         valComb= tk.StringVar()
         valComb.set(valores[0])
-        guardCombo= ttk.Combobox(self.mainWin, values= valores, state= "readonly", textvariable= valComb)
+        guardCombo= ttk.Combobox(mainWin, values= valores, state= "readonly", textvariable= valComb)
         guardCombo.place(x= 330, y= 316.5)
 
         lengVar= tk.IntVar()
         lengVar.set(1)
 
-        lenguajeEn= tk.Radiobutton(self.mainWin, text= "English", variable= lengVar, value= 1, takefocus= False)
+        lenguajeEn= tk.Radiobutton(mainWin, text= "English", variable= lengVar, value= 1, takefocus= False)
         lenguajeEn.place(x= 395, y= 5)
 
-        lenguajeEs= tk.Radiobutton(self.mainWin, text= "Español", variable= lengVar, value= 2, takefocus= False)
+        lenguajeEs= tk.Radiobutton(mainWin, text= "Español", variable= lengVar, value= 2, takefocus= False)
         lenguajeEs.place(x= 395, y= 30)
 
-        applyBut= tk.Button(self.mainWin, fg= "white", width= 10, bg= "#794ECF", text= "Apply", takefocus= False, command= audiobookCore)# Aún necesitamos agregar un método para validar el formulario de la app.
+        applyBut= tk.Button(mainWin, fg= "white", width= 10, bg= "#794ECF", text= "Apply", takefocus= False, command= audiobookCore)# Aún necesitamos agregar un método para validar el formulario de la app.
         applyBut.place(x= 196, y= 370)
 
         # Label to github repository
-        labelGit= tk.Label(self.mainWin, text= "Repository of the program:", font= ("jost", 10))
+        labelGit= tk.Label(mainWin, text= "Repository of the program:", font= ("jost", 10))
         labelGit.place(x= 130, y= 525)
 
         # Button to repository
-        butGit= tk.Button(self.mainWin, width= 10, bg= "#794ECF", fg= "white", text= "Repository", takefocus= False, command= repoGit)
+        butGit= tk.Button(mainWin, width= 10, bg= "#794ECF", fg= "white", text= "Repository", takefocus= False, command= repoGit)
         butGit.place(x= 290, y= 523)
 
 
-        self.mainWin.mainloop()
+        mainWin.mainloop()
